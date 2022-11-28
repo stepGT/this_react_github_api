344d419f0c23d5b185bd722e6cdbe1ab784caca5
Andrey Zuev <stepgt@mail.ru>
2022-11-28 10:58:25 +1000

    Added pages && routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/FavoritesPage';
+import FavoritesPage from './pages/HomePage';
+
 const App = () => {
-  return <div className="font-bold">App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+    </Routes>
+  );
 };
 
 export default App;
