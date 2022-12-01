70cc1bc84b45b81d6679e35b95f9475f8775ea2d
Andrey Zuev <stepgt@mail.ru>
2022-12-01 11:24:20 +1000

    Rendered data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,13 +4,14 @@ import { useSearchUsersQuery } from '../store/github/github.api';
 
 const HomePage = () => {
   const [search, setSearch] = useState('');
+  const [dropdown, setDropdown] = useState(false);
   const debounced = useDebounce(search);
   const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
     skip: debounced.length < 3,
   });
   useEffect(() => {
-    console.log(debounced);
-  }, [debounced]);
+    setDropdown(debounced.length > 3 && data?.length! > 0)
+  }, [debounced, data]);
   return (
     <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
       {isError && <p className="text-center text-red-600">Error!</p>}
@@ -22,11 +23,20 @@ const HomePage = () => {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas totam nulla dolore
-          repellendus exercitationem doloribus iste eos minima at asperiores expedita, suscipit
-          animi? Magni consequatur dolore ipsa maiores quidem nemo.
-        </div>
+        {dropdown && (
+          <ul className="overflow-y-scroll list-none absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
+            {isLoading && <p className="text-center">Loading...</p>}
+            {data?.map((item) => {
+              return (
+                <li
+                  key={item.id}
+                  className="cursor-pointer py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors">
+                  {item.login}
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
     </div>
   );
