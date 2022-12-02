4d37d5055c0867e70acc6ba5387f2151bf205c43
Andrey Zuev <stepgt@mail.ru>
2022-12-02 13:56:23 +1000

    Styled favorites pages && Refactored state

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,5 +1,22 @@
+import { useAppSelector } from '../hooks/redux';
+
 const FavoritesPage = () => {
-  return <div>FavoritesPage</div>;
+  const { favourites } = useAppSelector((state) => state.github);
+  if (favourites.length === 0) return <p className="text-center">No items.</p>;
+  //
+  return (
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      <ul className="list-none">
+        {favourites.map((item) => (
+          <li key={item}>
+            <a href={item} target="_blank">
+              {item}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default FavoritesPage;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { githubAPI } from './github/github.api';
+import { githubReducer } from './github/github.slice';
 
 export const store = configureStore({
   reducer: {
     [githubAPI.reducerPath]: githubAPI.reducer,
+    github: githubReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubAPI.middleware),
 });
 
 setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
